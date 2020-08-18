8bd07cf374d7e50709beda65244155f414f18bc0
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-08-18 11:44:27 +0530

    added categories, many to many associations

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
+  resources :categories, except: [:destroy]
 end
